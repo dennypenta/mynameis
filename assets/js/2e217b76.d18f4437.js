2bab00357fe9dd4edcec30307ad07100ccb90661
Denis <candyboobers@gmail.com>
2025-06-02 00:00:11 +0200

    Deploy website - based on b496679f4c6d459f32850648e79a541c996898ed

---
+++ assets/js/2e217b76.d18f4437.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmynameis=self.webpackChunkmynameis||[]).push([[682],{304:e=>{e.exports=JSON.parse('{"permalink":"/mynameis/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/b7209ac4.a4624de9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmynameis=self.webpackChunkmynameis||[]).push([[4],{3203:e=>{e.exports=JSON.parse('{"permalink":"/mynameis/blog/tags/software","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
